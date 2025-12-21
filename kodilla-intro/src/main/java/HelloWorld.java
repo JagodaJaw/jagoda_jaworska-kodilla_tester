52d05f2353b77200db898f300538dedcb2fd8795
Jagoda Jaworska <jaworska.jagoda@o2.pl>
2025-12-21 23:12:41 +0100

    Add HelloWorld and basic prints

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Jagoda");
+        System.out.println(31);
+        System.out.println('J');
+    }
+}
