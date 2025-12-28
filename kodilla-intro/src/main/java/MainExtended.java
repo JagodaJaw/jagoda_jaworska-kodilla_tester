aaafa570222ce8c7bec623e04722b74d064b4bd9
Jagoda Jaworska <jaworska.jagoda@o2.pl>
2025-12-29 00:06:59 +0100

    Add Main and MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Jagoda";
+        String surname = "Jaworska";
+        int myAge = 31;
+        double height = 1.6;
+        char name = 'J';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+        System.out.println(surname);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(name);
+        System.out.println(isGoodProgrammer);
+    }
+}
