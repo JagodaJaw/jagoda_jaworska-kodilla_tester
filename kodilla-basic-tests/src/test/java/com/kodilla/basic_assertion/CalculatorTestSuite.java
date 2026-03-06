dcc8018da861de49bf50c23ee802762e63f12b25
Jagoda Jaworska <jaworska.jagoda@o2.pl>
2026-03-06 21:17:15 +0100

    Add eunit tests for the calculator improvement.

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -1,43 +1,58 @@
 package com.kodilla.basic_assertion;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTestSuite {
 
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setup() {
+        calculator = new Calculator();
+    }
+
     @Test
-    public void testSubtract() {
-        Calculator calculator = new Calculator();
-
+    public void shouldSubtractPositiveNumbers() {
         double result = calculator.subtract(10, 4);
-
         assertEquals(6, result, 0.001);
     }
 
     @Test
-    public void testSquarePositiveNumber() {
-        Calculator calculator = new Calculator();
-
-        double result = calculator.square(5);
-
-        assertEquals(25, result, 0.001);
+    public void shouldSubtractNegativeNumbers() {
+        double result = calculator.subtract(-10, -4);
+        assertEquals(-6, result, 0.001);
     }
 
     @Test
-    public void testSquareZero() {
-        Calculator calculator = new Calculator();
-
-        double result = calculator.square(0);
-
+    public void shouldSubtractNumbersResultZero() {
+        double result = calculator.subtract(5, 5);
         assertEquals(0, result, 0.001);
     }
 
     @Test
-    public void testSquareNegativeNumber() {
-        Calculator calculator = new Calculator();
+    public void shouldSubtractPositiveAndNegative() {
+        double result = calculator.subtract(10, -5);
+        assertEquals(15, result, 0.001);
+    }
 
+    @Test
+    public void shouldSquarePositiveNumber() {
+        double result = calculator.square(5);
+        assertEquals(25, result, 0.001);
+    }
+
+    @Test
+    public void shouldSquareZero() {
+        double result = calculator.square(0);
+        assertEquals(0, result, 0.001);
+    }
+
+    @Test
+    public void shouldSquareNegativeNumber() {
         double result = calculator.square(-4);
-
         assertEquals(16, result, 0.001);
     }
 }
