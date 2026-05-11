987997f55a474a017fcd490447019e7bc90b81f4
Jagoda Jaworska <jaworska.jagoda@o2.pl>
2026-05-12 00:00:03 +0200

    Add module 10.4.

---
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerMvcTest.java
@@ -0,0 +1,63 @@
+package com.kodilla.rest.controller;
+
+import com.kodilla.rest.domain.BookDto;
+import com.kodilla.rest.service.BookService;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(BookController.class)
+public class BookControllerMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    public void shouldFetchBooks() throws Exception {
+        //given
+        List<BookDto> booksList = new ArrayList<>();
+        booksList.add(new BookDto("title 1", "author 1"));
+        booksList.add(new BookDto("title 2", "author 2"));
+        Mockito.when(bookService.getBooks()).thenReturn(booksList);
+
+        //when & then
+        mockMvc.perform(MockMvcRequestBuilders.get("/books")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(2)));
+    }
+
+    @Test
+    public void shouldAddBook() throws Exception {
+        //given
+        BookDto bookDto = new BookDto("title 1", "author 1");
+        Gson gson = new Gson();
+        String jsonContent = gson.toJson(bookDto);
+
+        //when & then
+        mockMvc.perform(MockMvcRequestBuilders.post("/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .content(jsonContent))
+                .andExpect(MockMvcResultMatchers.status().is(200));
+        Mockito.verify(bookService).addBook(bookDto);
+    }
+}
